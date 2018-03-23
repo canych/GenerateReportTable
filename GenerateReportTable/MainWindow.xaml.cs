36aa400876e31b9a007d5671a9f39eaf6cd94648
Александр <canych@ukr.net>
2018-03-23 20:33:56 +0300

    Создание данных для таблички

--- GenerateReportTable/MainWindow.xaml.cs
+++ GenerateReportTable/MainWindow.xaml.cs
@@ -24,5 +24,34 @@ namespace GenerateReportTable
         {
             InitializeComponent();
         }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            // Для оценок
+            List<int> marks = new List<int>();
+
+            Random r = new Random();
+            
+            for (int i = 0; i < 15; i++)
+            {
+                marks.Add(r.Next(4, 6));
+            }
+
+            // Количество троек
+            int count = r.Next(2, 3);
+
+            for (int i = 0; i < count; i++)
+            {
+                marks[r.Next(0, 15)] = 3;
+            }
+
+            // Создание и заполнение списка для создания таблички
+            List<Report> table = new List<Report>();
+
+            for (int i = 1; i < 16; i++)
+            {
+                table.Add(new Report(i, marks[i - 1]));
+            }
+        }
     }
 }

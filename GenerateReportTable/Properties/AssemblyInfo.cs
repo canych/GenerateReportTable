d14794d3030723b5ef3ac7c8043a4d4fc2e75f72
Александр <canych@ukr.net>
2018-03-25 16:35:57 +0300

    Сборка инсталлятора

--- GenerateReportTable/Properties/AssemblyInfo.cs
+++ GenerateReportTable/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GenerateReportTable")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © canych 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
